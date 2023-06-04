397818765f7b8ab6110332d07cf587d4c6692545
乐乡 <yilechen.cyl@digital-engine.com>
2023-06-05 00:01:56 +0800

    "update .gitignore"

--- test-service/src/main/java/com/example/test/Test.java
+++ test-service/src/main/java/com/example/test/Test.java
@@ -1,6 +1,9 @@
-package com.example.test;/**
+package com.example.test;
+
+/**
  * @author chenyile
  * @date 2023/2/22  12:35
- * @description: 
- */public class Test {
+ */
+public class Test {
+
 }
